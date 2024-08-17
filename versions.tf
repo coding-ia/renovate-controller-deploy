f7ba81256240d9911dcf1577c9884e49fa40bd11
coding-ia <137450628+coding-ia@users.noreply.github.com>
2024-08-17 13:05:46 -0700

    Rename provider.tf to version.tf

--- versions.tf
+++ versions.tf
@@ -1,12 +1,10 @@
 terraform {
+  required_version = ">= 1.3.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.61"
     }
   }
-}
-
-provider "aws" {
-  region = "us-east-2"
 }
