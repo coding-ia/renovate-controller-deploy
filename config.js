b8c96cfb868ca4f9ee5d55f00e613e065ec64833
bjunker <brian.junker@gmail.com>
2025-02-27 15:04:20 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     extends: ['config:recommended', ':disableDependencyDashboard', 'group:allNonMajor'],
   },
   onboardingConfigFileName: '.github/renovate.json',
-  repositories: ['{{.Repository}}'],
+  repositories: [{{range $index, $repo := .Repositories}}{{if $index}}, {{end}}"{{$repo}}"{{end}}],
   gitAuthor: 'Development Bot <dev-bot@my-software-company.com>',
   username: 'Coding-IA Test[bot]'
 }
