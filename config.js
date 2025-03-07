9478a1172f6f9883a59c267c8a4070138830e072
bjunker <brian.junker@gmail.com>
2025-03-07 10:22:13 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 module.exports = {
   autodiscover: true,
   endpoint: '{{.Endpoint}}',
-  gitAuthor: 'Development Bot <dev-bot@my-software-company.com>',
   onboardingConfig: {
     extends: ['config:recommended', ':disableDependencyDashboard', 'group:allNonMajor'],
   },
   onboardingConfigFileName: '.github/renovate.json',
   platform: 'github',
   token: '{{.InstallationToken}}',
-  username: 'Coding-IA Test[bot]'
 }
